1ab41c15b19c430ca5bc3898497eabdaa443b1e0
huhao666 <782044678@qq.com>
2022-08-22 17:33:19 +0800

    新进度

--- chapter02/part01/src/04_super.ts
+++ chapter02/part01/src/04_super.ts
@@ -1,6 +1,8 @@
 (function () {
     class Animal{
+
         name: string;
+        
         constructor(name: string){
             this.name = name;
         }
@@ -11,7 +13,7 @@
 
     class Dog extends Animal{
 
-        age: number
+        age: number;
 
         constructor(name: string, age: number){
             //如果在子类中写了构造函数，在子类构造函数中必须对父类的构造函数进行调用
