e53d391862ff5ac5646460685a97b381145cecb9
huhao666 <huhaoman@gmail.com>
2022-08-25 22:12:49 +0800

    新进度

--- chapter02/part01/src/05_抽象类.ts
+++ chapter02/part01/src/05_抽象类.ts
@@ -4,11 +4,8 @@
         以abstract 开头的类是抽象类
             抽象类和其他类区别不大，只是不能用来创建对象
             抽象类就是专门用来被继承的类
-
             抽象类中可以添加抽象方法
-    
     */
-    
     abstract class Animal{
         name: string;
         constructor(name: string){
@@ -26,24 +23,22 @@
     }
 
     class Dog extends Animal{
-
         sayHello() {
             console.log('汪汪汪~~')
         }
     }
 
-    
     class Cat extends Animal{
-
         sayHello() {
             console.log('喵喵喵~~')
         }
     }
 
     const dog = new Dog("旺财");
+    console.log(dog)
     dog.sayHello();
 
 
-   // const an = new Animal();//无法创建抽象类的实例
+   // const an = new Animal();//无法创建抽象类的实例，会报错
     dog.sayHello();
 })()
