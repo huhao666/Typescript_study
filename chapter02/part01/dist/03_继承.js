7d97e8765805b4a7737d7c5e5b1132c1bdd2e708
huhao666 <782044678@qq.com>
2022-08-26 15:42:24 +0800

    新进度

--- chapter02/part01/dist/03_继承.js
+++ chapter02/part01/dist/03_继承.js
@@ -14,10 +14,8 @@
         Dog extends Animal
             - 此时，Animal被称为父类，Dog被称为子类
             - 使用继承后，子类将会拥有父类所有的方法和属性
-            - 通过继承可以将多个类中共有的代码写在一个父类，
-                这样只需要写一次即可让所有的子类都同时拥有父类中的属性和方法
-            - 如果子类中添加了和父类相同的方法，则子类方法会覆盖掉父类的方法
-                这种子类覆盖掉父类方法的形式，我们称为重写
+            - 通过继承可以将多个类中共有的代码写在一个父类，这样只需要写一次即可让所有的子类都同时拥有父类中的属性和方法
+            - 如果子类中添加了和父类相同的方法，则子类方法会覆盖掉父类的方法，称为重写
     */
     //定义一个表示狗的类
     //使Dog类继承Animal类
@@ -41,6 +39,18 @@
     // 定义一个猫的类
     //使Cat类继承Animal类
     class Cat extends Animal {
+        // name: string;
+        // age: number;
+        // constructor(name: string, age: number){
+        //     this.name = name;
+        //     this.age = age;
+        // }
+        // sayHello(){
+        //     console.log('喵喵喵!!');
+        // }
+        sayHello() {
+            console.log('喵喵喵!!');
+        }
     }
     const dog = new Dog('旺财', 5);
     const cat = new Cat('咪咪', 3);
@@ -53,4 +63,5 @@
 /* OCP原则
     OPEN CLOSE开闭原则
     对扩展开放，对修改关闭
+    不要轻易改别人代码，改别人写好的类
 */ 
--- chapter02/part01/dist/05_抽象类.js
+++ chapter02/part01/dist/05_抽象类.js
@@ -4,9 +4,7 @@
         以abstract 开头的类是抽象类
             抽象类和其他类区别不大，只是不能用来创建对象
             抽象类就是专门用来被继承的类
-
             抽象类中可以添加抽象方法
-    
     */
     class Animal {
         constructor(name) {
@@ -24,7 +22,8 @@
         }
     }
     const dog = new Dog("旺财");
+    console.log(dog);
     dog.sayHello();
-    // const an = new Animal();//无法创建抽象类的实例
+    // const an = new Animal();//无法创建抽象类的实例，会报错
     dog.sayHello();
 })();
